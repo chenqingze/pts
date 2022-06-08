8330c1cea8efed322c44871e286ef4bfe23f7566
ChenQingze <chenqingze107@gmail.com>
2022-06-08 14:12:35 +0800

    update

--- src/app/pages/daq/daq.module.ts
+++ src/app/pages/daq/daq.module.ts
@@ -8,6 +8,12 @@ import { CertifiedApiComponent } from './certified-api/certified-api.component';
 import { CertifiedOperatorComponent } from './certified-operator/certified-operator.component';
 import {NzTableModule} from "ng-zorro-antd/table";
 import {NzPageHeaderModule} from "ng-zorro-antd/page-header";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzSelectModule} from "ng-zorro-antd/select";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzDatePickerModule} from "ng-zorro-antd/date-picker";
+import {NzInputModule} from "ng-zorro-antd/input";
+import {SharedModule} from "../../shared/shared.module";
 
 
 @NgModule({
@@ -21,7 +27,13 @@ import {NzPageHeaderModule} from "ng-zorro-antd/page-header";
         CommonModule,
         DaqRoutingModule,
         NzTableModule,
-        NzPageHeaderModule
+        NzPageHeaderModule,
+        NzFormModule,
+        NzSelectModule,
+        NzButtonModule,
+        NzDatePickerModule,
+        NzInputModule,
+        SharedModule
     ]
 })
 export class DaqModule {
